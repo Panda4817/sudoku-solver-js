09c840e1c009071eebba1117af6a222567eb1fa2
kanta <kanta.munton@gmail.com>
2021-04-28 14:08:40 +0100

    Added unit tests for validate and checkRowPlacement functions

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,76 @@
 class SudokuSolver {
+	validate(puzzleString) {
+		if (puzzleString.length != 81) {
+			return false;
+		}
+		for (let index = 0; index < 81; index++) {
+			const char = puzzleString[index];
+			if (char == ".") {
+				continue;
+			}
+			const n = parseInt(char);
+			if (isNaN(n)) {
+				return false;
+			} else if (n < 1 || n > 9) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-  validate(puzzleString) {
-  }
+	checkRowPlacement(puzzleString, row, column, value) {
+		const rows = {
+			A: 0,
+			B: 1,
+			C: 2,
+			D: 3,
+			E: 4,
+			F: 5,
+			G: 6,
+			H: 7,
+			I: 8,
+		};
+		if (rows.hasOwnProperty(row) == false) {
+			return false;
+		}
+		if (column < 1 || column > 9) {
+			return false;
+		}
+		row = rows[row];
+		let r = 0;
+		let row_string = "";
+		for (
+			let index = 0;
+			index < puzzleString.length;
+			index += 9
+		) {
+			if (r != row) {
+				r++;
+				continue;
+			}
+			row_string = puzzleString.substr(index, 9);
+			break;
+		}
+		if (row_string[column - 1] != ".") {
+			return false;
+		}
+		for (let index = 0; index < 9; index++) {
+			const char = row_string[index];
+			if (char == ".") {
+				continue;
+			}
+			if (char == value) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-  checkRowPlacement(puzzleString, row, column, value) {
+	checkColPlacement(puzzleString, row, column, value) {}
 
-  }
+	checkRegionPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
-
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+	solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,43 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+const solver = new Solver();
 
-suite('UnitTests', () => {
+suite("UnitTests", () => {
+	suite("Tests for validate function", function () {
+		test("valid puzzle input", function () {
+			let puzzle_input =
+				"1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+			assert.equal(solver.validate(puzzle_input), true);
+		});
+
+		test("invalid characters in puzzle input", function () {
+			let puzzle_input =
+				"1.5..2.84..63.12.7.2..5.....&..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+			assert.equal(solver.validate(puzzle_input), false);
+		});
+
+		test("invalid puzzle input length", function () {
+			let puzzle_input =
+				"1.5..2.84..63.12.7.2..5...9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+			assert.equal(solver.validate(puzzle_input), false);
+		});
+	});
+
+    suite("Tests for checkRowPlacement function", function () {
+		test("valid row placement", function () {
+			let puzzle_input =
+				"1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+			assert.equal(solver.checkRowPlacement(puzzle_input, "A", 2, 3), true);
+		});
+
+		test("invalid row placement", function () {
+			let puzzle_input =
+				"1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+			assert.equal(solver.checkRowPlacement(puzzle_input, "A", 2, 2), false);
+		});
+	});
+
 
 });
